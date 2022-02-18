b80494e200a1a5ee1effd60e5e0380182b140295
Tim Rudenko <73766495+TRudenko22@users.noreply.github.com>
2022-02-17 23:52:39 -0500

    Added function descriptions.

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ app = typer.Typer()
 
 @app.command()
 def insert(field1, field2, field3, field4) -> None:
+    """ Insert a record into JSON file.
+    """
     record = {col_1: field1, col_2: field2, col_3: field3, col_4: field4}
 
     data = open_db()
@@ -20,6 +22,9 @@ def insert(field1, field2, field3, field4) -> None:
 
 @app.command()
 def qn(name) -> None:
+    """ Query by string.
+    """
+    
     data = open_db()
 
     query = []
@@ -38,12 +43,18 @@ def qn(name) -> None:
 
 @app.command()
 def show() -> None:
+    """ Show JSON data in fancy grid format.
+    """
+    
     print(title)
     print(tabulate(open_db(), headers='keys', tablefmt='fancy_grid', showindex=True))
 
 
 @app.command()
 def delete(index):
+    """ Delete record in JSON file by index
+    """
+    
     data = open_db()
     data.pop(int(index))
     writeJSON(data,'db.json')
